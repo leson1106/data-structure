ba7007a5ba8d7b5379d81a0b366090d86b2bb85b
Son Le <itsonla@ghtk.co>
2023-08-17 09:07:20 +0700

    Simple hashset

---
+++ DataStructureJourney/SLHashSet/SLHashSet.h
@@ -0,0 +1,18 @@
+//
+//  SLHashSet.h
+//  SLHashSet
+//
+//  Created by Son Le on 17/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SLHashSet.
+FOUNDATION_EXPORT double SLHashSetVersionNumber;
+
+//! Project version string for SLHashSet.
+FOUNDATION_EXPORT const unsigned char SLHashSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLHashSet/PublicHeader.h>
+
+
---
+++ DataStructureJourney/SLHashSet/SLHashSet.swift
@@ -0,0 +1,41 @@
+//
+//  SLHashSet.swift
+//  SLHashSet
+//
+//  Created by Son Le on 17/08/2023.
+//
+
+import Foundation
+
+///Simple HashSet data structure with fixed-size and does not handle collisions
+public class SLHashSet {
+    private let defaultValue: Int = -1
+    private let size: Int
+    private var buckets: [Int]
+    
+    public init(_ size: Int) {
+        self.size = size
+        self.buckets = Array(repeating: defaultValue, count: size)
+    }
+    
+    public func add(_ key: Int) {
+        let index = hash(key)
+        buckets[index] = key
+    }
+    
+    public func remove(_ key: Int) {
+        let index = hash(key)
+        buckets[index] = defaultValue
+    }
+    
+    public func contains(_ key: Int) -> Bool {
+        let index = hash(key)
+        return buckets[index] != defaultValue
+    }
+}
+
+private extension SLHashSet {
+    func hash(_ value: Int) -> Int {
+        value % size
+    }
+}
