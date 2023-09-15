b4aea35bf156c84d37981edd0bd55335deb41614
Son Le <itsonla@ghtk.co>
2023-09-15 22:15:25 +0700

    Implementing linked list

--- DataStructureJourney/SLSequence/SLLinkedList.swift
+++ DataStructureJourney/SLSequence/SLLinkedList.swift
@@ -11,7 +11,7 @@ fileprivate class SLNode {
     fileprivate var value: Int
     fileprivate var next: SLNode?
     
-    fileprivate init(value: Int, next: SLNode? = nil) {
+    fileprivate init(_ value: Int, _ next: SLNode? = nil) {
         self.value = value
         self.next = next
     }
@@ -26,38 +26,111 @@ public class SLLinkedList {
     private var head: SLNode?
     private var _size: Int = 0
     
-    public init() {
-        head = SLNode.init()
+    public init() { }
+    
+    private func hadHead(with item: Int) -> Bool {
+        guard head == nil else { return false }
+        head = SLNode(item)
+        _size = 1
+        return true
     }
     
+    private func growSize() { _size += 1 }
+    private func shrinkSize() { _size -= 1; if _size < 0 { _size = 0 } }
+    
     ///Return the value at specific index
+    ///Time complexity `O(n)`
     public func valueAt(_ index: Int) -> Int? {
-        nil
+        guard index == 0 && head != nil else { return head?.value }
+        
+        var _index = 0
+        var temp = head
+        
+        while temp?.next != nil {
+            if _index == index { return temp?.value }
+            temp = temp?.next
+            _index += 1
+        }
+        
+        return nil
     }
     
     ///Add an item to the front of the list
+    ///Time complexity `O(1)`
     public func pushFront(_ item: Int) {
+        guard hadHead(with: item) else { return }
         
+        let newNode = SLNode(item)
+        newNode.next = head
+        head = newNode
+        
+        growSize()
     }
     
     ///Remove front item and return it's value
+    ///Time complexity `O(1)`
     public func popupFront() -> Int? {
-        nil
+        var temp = head
+        let tempVal = temp?.value
+        temp = temp?.next
+        head = temp
+        
+        shrinkSize()
+        
+        return tempVal
     }
     
     ///Add an item at the end
+    ///Time complexity `O(n)`
     public func pushBack(_ item: Int) {
+        var temp = head
+        while temp?.next != nil {
+            temp = temp?.next
+        }
+        let newNode = SLNode(item)
+        temp?.next = newNode
         
+        growSize()
     }
     
     ///Remove end item and return its value
     public func popBack() -> Int? {
-        nil
+        var dummy: SLNode? = SLNode(-1, head)
+        var temp = dummy
+        
+        while temp?.next != nil {
+            if temp?.next?.next == nil {
+                temp?.next
+            }
+        }
+        
+        shrinkSize()
+        
+        return temp?.value
     }
     
     ///Insert new item at specific index
+    ///Time complexity `O(n)`
     public func insert(_ item: Int, at index: Int) {
+        if index == size {
+            pushBack(item)
+            return
+        }
         
+        guard index < size  else {
+            fatalError("index out of range")
+        }
+        
+        var _index = 0
+        var temp = head
+        while temp?.next != nil {
+            if _index == index {
+                
+            }
+            
+            temp = temp?.next
+            _index += 1
+        }
     }
     
     ///Remove item at index
