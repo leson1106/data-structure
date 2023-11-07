ee1e16977cf00ef78316e7aae8d37671cad392f4
Son Le <itsonla@ghtk.co>
2023-11-07 15:38:05 +0700

    Minor

---
+++ DataStructureJourney/SLQueue/SLHeap.swift
@@ -0,0 +1,16 @@
+//
+//  SLHeap.swift
+//  SLQueue
+//
+//  Created by Son Le on 11/10/2023.
+//
+
+import Foundation
+
+///getMax O(1)
+///insert O(log n)
+///delete O(log n)
+///change value O(log n)
+public class SLHeap {
+    
+}
